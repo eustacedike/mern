bd0d8c9d96d2a78c9116857b9f0367ad38a19b34
eustacedike <98954452+eustacedike@users.noreply.github.com>
2022-12-04 12:36:33 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://eustace:balleralert@cluster0.8xhhc2x.mongodb.net/test",
+  mongoURI: "mongodb+srv://<username>:<password>@cluster0.8xhhc2x.mongodb.net/test",
   secretOrKey: "secret"
 };
